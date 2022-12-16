c295563c7563bd559ab16bcfe6730bb4c14ca071
Renan Mougenot Breviglieri Silva <mougenot.renan@gmail.com>
2022-12-15 20:01:20 -0700

    Dec 15  2022

---
+++ js/app.js
@@ -0,0 +1,28 @@
+//Variable
+let button = document.getElementById("gerar");
+
+//Function to generate the 5 random numbers
+function get_numbers(){
+
+    //Container to hold random numbers
+    let number_container = [];
+
+    //Generate 5 random numbers
+    for(let i = 0; i < 5; i++){
+        //Get random numbers up to 100
+        let random = Math.floor(Math.random() * 100);
+        
+        //Push random number
+        number_container.push(random);
+    }
+
+    //Display result area
+    document.getElementById("result").style.display = "block";
+
+    //Display random numbers
+    document.getElementById("fill").innerHTML = number_container;
+
+}
+
+//Add event listener on button click
+button.addEventListener("click",get_numbers);
