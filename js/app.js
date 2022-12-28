be4adab9533bc4350ad9b000d9aeb6beab7f73d3
Renan Mougenot Breviglieri Silva <mougenot.renan@gmail.com>
2022-12-27 20:49:15 -0700

    Dec 27  2022

--- js/app.js
+++ js/app.js
@@ -14,18 +14,48 @@ function get_numbers(){
 
         //Ignore 0
         if(random == 0){
-            random = Math.floor(Math.random() * 99);;
+            random = Math.floor(Math.random() * 99);
         }
 
         //Push random number
         number_container.push(random);
     }
 
+    unique(number_container);
+
     //Display result area
     document.getElementById("result").style.display = "block";
 
     //Display random numbers
     document.getElementById("fill").innerHTML = number_container;
+
+}
+
+//Function to make sure every number is unique
+function unique(array){
+
+    array.sort((a,b)=>a-b);
+
+    for(let i = 0; i <= array.length; i++){
+
+        if(array[i-1] === array[i]){
+
+            console.log(`same number found. 
+                        NUMBER[I-1] PREVIOUS: ${array[i-1]}
+                        NUMBER[I] PREVIOUS: ${array[i]}`
+                        );
+
+            array[i] = Math.floor(Math.random() * 99);
+
+            console.log(`same number found. 
+                        NUMBER[I-1] AFTER: ${array[i-1]}
+                        NUMBER[I] AFTER: ${array[i]}`
+                        );
+
+            unique(array);
+        }
+
+    }
 
 }
 
